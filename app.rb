e86007fbf24b71a6c7c20a83feab7b9eb9043646
Ashley Bland <ashleyjbland@gmail.com>
2019-07-31 22:23:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ class App < Sinatra::Base
   end
 
   get '/second_exercise' do
-    session[1]
+    session[:id] = session[1]
     "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
   end
 
